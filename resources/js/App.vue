025943c9e1012495fc858a88099575535da7605f
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-03 00:29:00 +0200

    Add global loader

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="grid grid-rows-layout grid-cols-1">
-        <app-header/>
-        <main class="bg-blue-100 flex relative justify-center py-20 row-start-2 row-end-3">
-            <preview/>
-            <settings/>
+        <loading ref="loading" />
+        <app-header />
+        <main
+            class="bg-blue-100 flex relative justify-center py-20 row-start-2 row-end-3"
+        >
+            <preview @background-image-loading="toggleLoader" />
+            <settings />
         </main>
-        <app-footer/>
-        <vue-snotify/>
+        <app-footer />
+        <vue-snotify />
     </div>
 </template>
 
 <script>
+import Loading from '~/components/Loading'
+
 import Preview from '~/components/Preview'
 import Settings from '~/components/Settings'
 
@@ -19,10 +24,21 @@ import AppFooter from '~/components/Footer'
 
 export default {
     components: {
+        Loading,
         Settings,
         Preview,
         AppHeader,
         AppFooter
+    },
+
+    methods: {
+        toggleLoader (isLoading) {
+            return isLoading ? this.$loading.start() : this.$loading.finish()
+        }
+    },
+
+    mounted () {
+        this.$loading = this.$refs.loading
     }
 }
 </script>
--- resources/js/directives.js
+++ resources/js/directives.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 
 Vue.directive('background-image', {
-    componentUpdated (el, binding) {
+    componentUpdated(el, binding, vnode) {
+        vnode.context.$emit('background-image-loading', true)
         el.style.opacity = '0'
-
         const image = document.createElement('img')
         image.addEventListener('load', () => {
             el.style.backgroundImage = 'url(' + binding.value + ')'
             el.style.opacity = '1'
+            vnode.context.$emit('background-image-loading', false)
         })
         image.src = binding.value
     }
