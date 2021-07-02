1fc424b28c314bff961fff88362ce88e8ecba96a
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-02 23:35:02 +0200

    Fix: Bug with endless drawing

--- resources/js/components/Settings.vue
+++ resources/js/components/Settings.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="relative bg-white rounded-lg text-left overflow-hidden shadow-md sm:max-w-744 sm:w-full">
+    <div
+        class="relative bg-white rounded-lg text-left overflow-hidden shadow-md sm:max-w-744 sm:w-full"
+    >
         <div class="p-5 sm:px-8 sm:py-10">
             <div class="mb-10">
-                <color-picker v-model="form.color"/>
+                <color-picker v-model="form.color" />
             </div>
             <div class="flex space-x-6 mb-10">
                 <div class="w-1/2">
                     <div class="mb-3 flex items-center justify-between">
-                        <h4 class="text-gray-900 font-medium uppercase">Pattern</h4>
+                        <h4 class="text-gray-900 font-medium uppercase">
+                            Pattern
+                        </h4>
                         <label class="custom-checkbox text-13 text-gray-300">
                             <input
                                 v-model="form.invert"
@@ -15,18 +19,30 @@
                                 name="position"
                                 :true-value="1"
                                 :false-value="0"
-                            >
-                            <span/>
+                            />
+                            <span />
                             Invert
                         </label>
                     </div>
-                    <inline-select v-model="form.pattern" :options="patterns" type="pattern" placeholder="Search patterns..."/>
-                    <range v-model="form.intensity" title="Pattern Intensity"/>
+                    <inline-select
+                        v-model="form.pattern"
+                        :options="patterns"
+                        type="pattern"
+                        placeholder="Search patterns..."
+                    />
+                    <range v-model="form.intensity" title="Pattern Intensity" />
                 </div>
                 <div class="w-1/2">
-                    <h4 class="text-gray-900 font-medium uppercase mb-3">Overlay</h4>
-                    <inline-select v-model="form.blend" :options="blendModes" type="blend" placeholder="Search overlay filters..."/>
-                    <range v-model="form.noise" title="Noise"/>
+                    <h4 class="text-gray-900 font-medium uppercase mb-3">
+                        Overlay
+                    </h4>
+                    <inline-select
+                        v-model="form.blend"
+                        :options="blendModes"
+                        type="blend"
+                        placeholder="Search overlay filters..."
+                    />
+                    <range v-model="form.noise" title="Noise" />
                 </div>
             </div>
 
@@ -37,22 +53,25 @@
                     download
                     @click="download"
                 >
-                    <img src="/images/down.svg" class="mr-2.5">
-                    Download  @1x image
+                    <img src="/images/down.svg" class="mr-2.5" />
+                    Download @1x image
                 </a>
-                <button class="border-l border-white rounded-r-lg flex items-center justify-center w-10 h-12 bg-blue-200">
-                    <img src="/images/arrow-down.svg">
+                <button
+                    class="border-l border-white rounded-r-lg flex items-center justify-center w-10 h-12 bg-blue-200"
+                >
+                    <img src="/images/arrow-down.svg" />
                 </button>
             </div>
         </div>
 
-        <patterns/>
+        <patterns />
     </div>
 </template>
 
 <script>
 import { mapMutations, mapGetters } from 'vuex'
 import _capitalize from 'lodash/capitalize'
+import _isEqual from 'lodash/isEqual'
 
 import InlineSelect from '~/components/Fields/InlineSelect'
 import Range from '~/components/Fields/Range'
@@ -77,10 +96,7 @@ export default {
     },
 
     computed: {
-        ...mapGetters([
-            'settings',
-            'backgroundUrl'
-        ]),
+        ...mapGetters(['settings', 'backgroundUrl']),
         blendModes () {
             return Object.keys(window.config.blendModes).map(key => ({
                 value: key,
@@ -90,7 +106,7 @@ export default {
         patterns () {
             return window.config.patterns.map(pattern => ({
                 value: pattern,
-                label: _capitalize(pattern.replace(/_/ig, ' '))
+                label: _capitalize(pattern.replace(/_/gi, ' '))
             }))
         }
     },
@@ -99,7 +115,9 @@ export default {
         form: {
             deep: true,
             handler () {
-                this.updatePreview()
+                if (!_isEqual(this.settings, this.form)) {
+                    this.updatePreview()
+                }
             }
         },
         settings: {
@@ -111,9 +129,7 @@ export default {
     },
 
     methods: {
-        ...mapMutations([
-            'updateSettings'
-        ]),
+        ...mapMutations(['updateSettings']),
         updatePreview () {
             this.updateSettings(this.form)
         },
