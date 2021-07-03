1df2f30ab67a4f155c3fa2bc1a1fb0656b44905e
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-03 11:44:56 +0200

    Add bind method to directive

--- resources/js/directives.js
+++ resources/js/directives.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 
 Vue.directive('background-image', {
+    bind (el, binding) {
+        el.style.opacity = '0'
+        const image = document.createElement('img')
+        image.addEventListener('load', () => {
+            el.style.backgroundImage = 'url(' + binding.value + ')'
+            el.style.opacity = '1'
+        })
+        image.src = binding.value
+    },
+
     componentUpdated (el, binding, vnode) {
         vnode.context.$emit('background-image-loading', true)
         el.style.opacity = '0'
