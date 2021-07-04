6c2ea1b7288d04e1006993e341ef49cac1a2030e
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-04 17:28:52 +0200

    Clear

--- resources/js/components/ShareButtons/Facebook.vue
+++ resources/js/components/ShareButtons/Facebook.vue
@@ -4,6 +4,7 @@
         url="https://bg.siteorigin.com"
         title="Background Image Generator - SiteOrigin"
         description="Create custom, tileable background images for your site"
+        quote="Create custom, tileable background images for your site"
         hashtags="SiteOrigin,WebDesign,BackgroundGenerator"
     >
         <div class="border-2 border-gray-100 rounded-lg px-4 h-10 flex items-center text-sm font-medium text-gray-500 hover:bg-gray-50 transition duration-200">
