6cf4a27f8057d99cb692aee078fbfa22bdfa4c0b
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-04 15:50:05 +0200

    Feat: Install vue-social-sharing

--- resources/js/components/ShareButtons/Facebook.vue
+++ resources/js/components/ShareButtons/Facebook.vue
@@ -1,26 +1,11 @@
 <template>
-    <div
-        class="fb-like"
-        data-href="https://bg.siteorigin.com"
-        data-layout="button_count"
-        data-action="like"
-        data-show-faces="false"
-        data-share="false"
-        hidden
-    />
+    <ShareNetwork
+        network="facebook"
+        url="https://bg.siteorigin.com"
+        title="Background Image Generator - SiteOrigin"
+        description="Create custom, tileable background images for your site"
+        hashtags="SiteOrigin,WebDesign,BackgroundGenerator"
+    >
+        Facebook
+    </ShareNetwork>
 </template>
-
-<script>
-export default {
-    mounted () {
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0]
-            if (d.getElementById(id)) return
-            js = d.createElement(s)
-            js.id = id
-            js.src = `//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3&appId=${process.env.MIX_FACEBOOK_APP_ID}`
-            fjs.parentNode.insertBefore(js, fjs)
-        }(document, 'script', 'facebook-jssdk'))
-    }
-}
-</script>
--- resources/js/components/ShareButtons/Twitter.vue
+++ resources/js/components/ShareButtons/Twitter.vue
@@ -1,35 +1,12 @@
 <template>
-    <a
-        href="https://twitter.com/share"
-        class="twitter-share-button"
-        data-url="https://bg.siteorigin.com"
-        data-text="Create custom, tileable background images for your site"
-        data-via="SiteOrigin"
-        data-related="SiteOrigin"
-        hidden
+    <ShareNetwork
+        network="twitter"
+        url="https://bg.siteorigin.com"
+        title="Background Image Generator - SiteOrigin"
+        description="Create custom, tileable background images for your site"
+        twitter-user="SiteOrigin"
+        hashtags="SiteOrigin,WebDesign,BackgroundGenerator"
     >
-        Tweet
-    </a>
+        Twitter
+    </ShareNetwork>
 </template>
-
-<script>
-export default {
-    mounted () {
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0],
-                t = window.twttr || {}
-            if (d.getElementById(id)) return t.widgets.load()
-            js = d.createElement(s)
-            js.id = id
-            js.src = 'https://platform.twitter.com/widgets.js'
-            fjs.parentNode.insertBefore(js, fjs)
-
-            t._e = []
-            t.ready = function (f) {
-                t._e.push(f)
-            }
-            return t
-        }(document, 'script', 'twitter-wjs'))
-    }
-}
-</script>
