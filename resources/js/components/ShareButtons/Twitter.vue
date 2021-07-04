017de0568e76324f9fe39c9aff02df4b705031aa
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2021-07-04 19:33:00 +0200

    Change twitter share text

--- resources/js/components/ShareButtons/Twitter.vue
+++ resources/js/components/ShareButtons/Twitter.vue
@@ -2,8 +2,7 @@
     <ShareNetwork
         network="twitter"
         url="https://bg.siteorigin.com"
-        title="Background Image Generator - SiteOrigin"
-        description="Create custom, tileable background images for your site"
+        title="Create custom, tileable background images for your site"
         twitter-user="SiteOrigin"
         hashtags="SiteOrigin,WebDesign,BackgroundGenerator"
     >
