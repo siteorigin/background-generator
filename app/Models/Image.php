28a8cd639acb09ea281275f03a13ab3b389f5655
vkazakevich <vkazakevich@users.noreply.github.com>
2021-07-12 19:18:40 +0000

    PHP CS Fixer

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Presenters\ImagePresenter;
 use \Imagick;
 use App\Casts\HexColorCast;
 use App\Image\CustomImagick;
+use App\Models\Presenters\ImagePresenter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use ImagickPixel;
